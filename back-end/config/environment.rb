87ed594a90be4cdc0419827089cd69c610a2b9cc
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-06-09 18:51:54 +0300

    update config

--- back-end/config/environment.rb
+++ back-end/config/environment.rb
@@ -1,56 +1,6 @@
-before do
-    content_type :json
-  end
-  
-  get '/users' do
-    users = User.all
-    users.to_json
-  end
-  
-  post '/users' do
-    user = User.create(name: params[:name])
-    user.to_json
-  end
-  
-  get '/foods' do
-    foods = Food.all
-    foods.to_json(include: [:likes])
-  end
-  
-  post '/foods' do
-    food = Food.create(
-      name: params[:name],
-      image: params[:image],
-      description: params[:description]
-    )
-    food.to_json
-  end
-  
-  post '/likes' do
-    like = Like.create(user_id: params[:user_id], food_id: params[:food_id])
-    like.to_json
-  end
-  
-  put '/foods/:id' do |id|
-    food = Food.find(id)
-    if food.update(
-         name: params[:name],
-         image: params[:image],
-         description: params[:description]
-       )
-      food.to_json
-    else
-      status 400
-      { message: "Failed to update food" }.to_json
-    end
-  end
-  
-  delete '/foods/:id' do |id|
-    food = Food.find(id)
-    if food.destroy
-      { message: "Food deleted" }.to_json
-    else
-      status 400
-      { message: "Failed to delete food" }.to_json
-    end
-  end
+ENV["RACK_ENV"] || = "development"
+
+require 'bundle/setup'
+Bundler.require(:default, ENV["RACK_ENV"])
+
+require_all 'app'
