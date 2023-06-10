5fda04cb38793ff10922f1151b02eb825bc7bf46
MullahStanley <mullah.kimani@student.moringaschool.com>
2023-06-10 10:59:11 +0300

    bundle error fix

--- back-end/app/controllers/application_controller.rb
+++ back-end/app/controllers/application_controller.rb
@@ -1,57 +1,62 @@
-before do
-    content_type :json
-  end
-  
-  get '/users' do
-    users = User.all
-    users.to_json
-  end
-  
-  post '/users' do
-    user = User.create(name: params[:name])
-    user.to_json
-  end
-  
-  get '/foods' do
-    foods = Food.all
-    foods.to_json(include: [:likes])
-  end
-  
-  post '/foods' do
-    food = Food.create(
-      name: params[:name],
-      image: params[:image],
-      description: params[:description]
-    )
-    food.to_json
-  end
-  
-  post '/likes' do
-    like = Like.create(user_id: params[:user_id], food_id: params[:food_id])
-    like.to_json
-  end
-  
-  put '/foods/:id' do |id|
-    food = Food.find(id)
-    if food.update(
-         name: params[:name],
-         image: params[:image],
-         description: params[:description]
-       )
-      food.to_json
-    else
-      status 400
-      { message: "Failed to update food" }.to_json
+class ApplicationController < Sinatra::Base
+    get "/" do
+        "hello world"
     end
-  end
-  
-  delete '/foods/:id' do |id|
-    food = Food.find(id)
-    if food.destroy
-      { message: "Food deleted" }.to_json
-    else
-      status 400
-      { message: "Failed to delete food" }.to_json
+    before do
+        content_type :json
     end
-  end
-  
+      
+      get '/users' do
+        users = User.all
+        users.to_json
+      end
+      
+      post '/users' do
+        user = User.create(name: params[:name])
+        user.to_json
+      end
+      
+      get '/foods' do
+        foods = Food.all
+        foods.to_json(include: [:likes])
+      end
+      
+      post '/foods' do
+        food = Food.create(
+          name: params[:name],
+          image: params[:image],
+          description: params[:description]
+        )
+        food.to_json
+      end
+      
+      post '/likes' do
+        like = Like.create(user_id: params[:user_id], food_id: params[:food_id])
+        like.to_json
+      end
+      
+      put '/foods/:id' do |id|
+        food = Food.find(id)
+        if food.update(
+             name: params[:name],
+             image: params[:image],
+             description: params[:description]
+           )
+          food.to_json
+        else
+          status 400
+          { message: "Failed to update food" }.to_json
+        end
+      end
+      
+      delete '/foods/:id' do |id|
+        food = Food.find(id)
+        if food.destroy
+          { message: "Food deleted" }.to_json
+        else
+          status 400
+          { message: "Failed to delete food" }.to_json
+        end
+      end
+      
+end
